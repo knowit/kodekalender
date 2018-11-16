4b11e5189286ede9f9536106a3f4bfb0178d2153
Alexander Bjerkan <alb@knowit.no>
2018-11-16 09:54:35 +0100

    feat: Simple leaderboard

--- client/src/Leaderboard.js
+++ client/src/Leaderboard.js
@@ -2,34 +2,59 @@ import React from 'react';
 import useDocumentTitle from '@rehooks/document-title';
 import { useQuery } from 'react-apollo-hooks';
 import gql from 'graphql-tag';
+import { css } from 'react-emotion';
 
+import Container from './components/Container';
 import Heading from './components/Heading';
 
 export default () => {
   useDocumentTitle('Kodekalender: Ledertavle');
 
-  const { data, error } = useQuery(ALL_USERS_QUERY);
+  const { data, error } = useQuery(LEADERBOARD_QUERY);
 
   return (
-    <>
+    <Container>
       <Heading>Ledertavle</Heading>
-      <table>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>Plass</th>
+            <th>Bruker</th>
+            <th>Luker</th>
+          </tr>
+        </thead>
         <tbody>
-          {data.allUsers.map(u => (
-            <tr key={u.email}>
-              <td>{u.email}</td>
+          {data.allUsers.map((user, index) => (
+            <tr key={user.nickname}>
+              <td>{index + 1}</td>
+              <td>
+                <a href={`https://github.com/${user.nickname}`}>
+                  {user.nickname}
+                </a>
+              </td>
+              <td>{user._solutionsMeta.count}</td>
             </tr>
           ))}
         </tbody>
       </table>
-    </>
+    </Container>
   );
 };
 
-const ALL_USERS_QUERY = gql`
-  query {
+const LEADERBOARD_QUERY = gql`
+  query leaderboard {
     allUsers {
-      email
+      nickname
+      picture
+      _solutionsMeta {
+        count
+      }
     }
   }
 `;
+
+const styles = {
+  table: css`
+    width: 100%;
+  `,
+};
